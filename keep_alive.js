642c21df90e648b4651af60568730ef77e04cc88
s1ksak <168342258+s1ksak@users.noreply.github.com>
2024-05-19 14:46:34 +0700

    Create keep_alive.js

---
+++ keep_alive.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js'); //import client from discord
+const keep_alive = require('./keep_alive.js')
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+    // check if message isn't from us
+    if (msg.author == client.user) {
+      return;
+    }
+    else if (msg.content === 'ping') {
+      msg.reply('Pong!');
+    }
+    else {
+        msg.reply(msg.content);
+    }
+});
+
+client.login(process.env.TOKEN); //login bot using token
